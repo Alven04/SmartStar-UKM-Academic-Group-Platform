1ca45ccd69d40ffd319267b7534878f3f008fb97
Alven04 <118118049+Alven04@users.noreply.github.com>
2023-01-30 17:40:17 +0800

    Added courseID
    attributes, actionPerformed and some minor changes

--- SmartStar/src/AddCourse.java
+++ SmartStar/src/AddCourse.java
@@ -7,9 +7,14 @@ import java.awt.Color;
 
 public class AddCourse extends JFrame implements ActionListener {
 	private JButton backBtn,confirmBtn;
+	private JRadioButton privacyPublic,privacyPrivate;
+	private ButtonGroup bg;
 	private Controller controller;
-	private JTextField courseName;
+	private JTextField courseName,courseID;
 	private JTextArea courseDescription;
+	private JScrollPane scrollPane;
+	private JFrame f;
+	
 	
 	public AddCourse(Controller controller) {
 		this.controller= controller;
@@ -20,28 +25,46 @@ public class AddCourse extends JFrame implements ActionListener {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		Container pane = getContentPane();
-		pane.setBackground(new Color(204,204,204));
+		pane.setBackground(Color.black);
 		pane.setLayout(new BorderLayout());
 		
-		JLabel label1 =new JLabel("SmartStar Course");
+		JLabel label1 =new JLabel("Add New Course");
 		label1.setForeground(Color.WHITE);
 		label1.setFont(new Font("Times New Roman", Font.PLAIN,16));
 		
-		JLabel lname =new JLabel("Course Name");
-		lname.setForeground(Color.RED.darker());
+		JLabel lname =new JLabel("Course Name :");
+		lname.setForeground(Color.BLACK);
 		lname.setFont(new Font("Times New Roman", Font.PLAIN,16));
 		
-		JLabel ldescription =new JLabel("Description");
-		ldescription.setForeground(Color.RED.darker());
+		JLabel courseIDLabel =new JLabel("Course ID :");
+		courseIDLabel.setForeground(Color.BLACK);
+		courseIDLabel.setFont(new Font("Times New Roman", Font.PLAIN,16));
+		
+		JLabel privacy =new JLabel("Privacy :");
+		privacy.setForeground(Color.BLACK);
+		privacy.setFont(new Font("Times New Roman", Font.PLAIN,16));
+		
+		JLabel ldescription =new JLabel("Description :");
+		ldescription.setForeground(Color.BLACK);
 		ldescription.setFont(new Font("Times New Roman", Font.PLAIN,16));
 		
-		courseName=new JTextField(20);
+		courseName=new JTextField(20); //courseName TextField
 		courseDescription=new JTextArea(5,30);
+		scrollPane=new JScrollPane(courseDescription);
+		courseID=new JTextField(10); //courseID TextField
 		
 		backBtn=new JButton("Back");
 		backBtn.setForeground(new Color(0,0,0));
 		confirmBtn=new JButton("Confirm");
 		confirmBtn.setForeground(new Color(0,0,0));
+		
+		bg=new ButtonGroup();
+		privacyPublic=new JRadioButton("Public");
+		privacyPublic.setForeground(new Color(0,0,0));
+		privacyPrivate=new JRadioButton("Private");
+		privacyPrivate.setForeground(new Color(0,0,0));
+		bg.add(privacyPublic);
+		bg.add(privacyPrivate);
 		
 		JPanel panel1a=new JPanel();
 		panel1a.add(label1);
@@ -53,27 +76,37 @@ public class AddCourse extends JFrame implements ActionListener {
 		panel1.add(panel1a, BorderLayout.CENTER);
 		panel1.setBackground(new Color(0,0,255));
 		
+		JPanel panel1b=new JPanel(); //Course ID
+		panel1b.add(courseIDLabel);
+		panel1b.add(courseID);
+		panel1b.setBackground(new Color(0,100,255));
 		
 		JPanel panel2=new JPanel(); //Course Name
-		panel2.setLayout(new FlowLayout());
 		panel2.add(lname);
 		panel2.add(courseName);
-		panel2.setBackground(new Color(0,100,155));
+		panel2.setBackground(new Color(0,100,255));
+		panel2.setSize(10,10);
 		
-		JPanel panel3=new JPanel(); //Course Description
-		panel3.setLayout(new FlowLayout());
-		panel3.add(ldescription);
-		panel3.add(courseDescription);
-		panel3.setBackground(new Color(0,100,155));
+		JPanel panel3=new JPanel(); //Course Name
+		panel3.add(privacy);
+		panel3.add(privacyPublic);
+		panel3.add(privacyPrivate);
+		panel3.setBackground(new Color(0,100,255));
 		
+		JPanel panel4=new JPanel(); //Course Description
+		panel4.add(ldescription);
+		panel4.add(scrollPane); //make the box fixed
+		panel4.setBackground(new Color(0,100,255));
 		
 		JPanel mainPanel=new JPanel();
-		mainPanel.setLayout(new BorderLayout());
-		mainPanel.setBackground(new Color(0,100,155));
+		mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.PAGE_AXIS));
+		mainPanel.setBackground(new Color(0,100,255));
+		mainPanel.add(panel1b);
+		mainPanel.add(panel2);
+		mainPanel.add(panel3);
+		mainPanel.add(panel4);
+		mainPanel.add(confirmBtn);
 		
-		mainPanel.add(panel2, BorderLayout.PAGE_START);
-		mainPanel.add(panel3, BorderLayout.CENTER);
-		mainPanel.add(confirmBtn,BorderLayout.SOUTH);
 		
 		pane.add(panel1, BorderLayout.PAGE_START);
 		pane.add(mainPanel,BorderLayout.CENTER);
@@ -85,14 +118,15 @@ public class AddCourse extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj=e.getSource();
-		this.setVisible(false);
 		if(obj.equals(backBtn)) {
+			this.setVisible(false);
 			controller.displayScreen(1);
 		}
 		if(obj.equals(confirmBtn)) {
 			int a=JOptionPane.showConfirmDialog(f, "Add Course?");
 			if(a==JOptionPane.YES_OPTION) {
-				JOptionPane.showMessageDialog(null, "Your course has been created","Course Created",JOptionPane.PLAIN_MESSAGE);;
+				JOptionPane.showMessageDialog(null, "Your course has been created","Course Created",JOptionPane.PLAIN_MESSAGE);
+				controller.addCourseId(courseID.getText());
 				controller.addCourseName(courseName.getText());
 				controller.addCourseDescription(courseDescription.getText());
 				controller.addCoursePrivacy(bg.getSelection().getActionCommand());
