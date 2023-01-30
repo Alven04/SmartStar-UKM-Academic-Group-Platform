f51cfb8e6d78b6874db033e80ab1ff5e0573467c
cutejoy <stuartfuyu0318@gmail.com>
2023-01-30 17:34:39 +0800

    Update Controller.java

--- SmartStar/src/Controller.java
+++ SmartStar/src/Controller.java
@@ -3,8 +3,7 @@ public class Controller {
 	private Course c=new Course(); //optimize this if possible (idk how)
 	
 	public Controller() {
-		Controller controller = new Controller(); //this as well
-		controller.displayScreen(1);
+		this.displayScreen(1);
 	}
 	
 	public void displayScreen(int num) {
@@ -14,7 +13,8 @@ public class Controller {
 			new AddCourse(this);
 		else if(num==3) 
 			new JoinCourse(this);
-		
+		else if (num==11)
+			new SplashScreen(this);
 	}
 	
 	public void addCourseName(String name) {
