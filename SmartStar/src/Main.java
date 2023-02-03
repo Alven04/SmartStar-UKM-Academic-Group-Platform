f72a69d398b2ae3d788cc4b228fb8bd93fb86b4b
cutejoy <stuartfuyu0318@gmail.com>
2023-02-03 11:00:39 +0800

    Fixed an issue on Main

--- SmartStar/src/Main.java
+++ SmartStar/src/Main.java
@@ -22,8 +22,8 @@ public class Main {
 	
 	private static void initialize() {
 		
-		initializeStudent("rebekah", "admin", "Rebekah", "USM", 1, "Information Technology");
-		initializeStudent("stuart", "handsome", "Stuart", "UKM", 2, "Computer Science");
+		initializeStudent("faris", "admin", "Faris", "USM", 1, "Information Technology");
+		initializeStudent("stuart", "handsome", "Stuart", "UM", 2, "Computer Science");
 		initializeStudent("alven", "alven", "Alvenlim", "UKM", 2, "Computer Science");
 		
 		//PLEASE DELETE BEFORE SUBMIT: allows sign in without entering username/password (for debug only)
@@ -33,9 +33,9 @@ public class Main {
 		initializeLecturer("siti", "12345", "Dr Siti", "UKM", "Professor");
 		initializeLecturer("rohizah", "12345", "Dr Rohizah", "UKM", "Assosciate Professor");
 
-		initializeCourse("TK2023", "OOSE", "Learn Software Development Life Cycle", "hafiz", new ArrayList<String>(Arrays.asList("daphne", "rebekah")));
-		initializeCourse("TK1143", "Programming", "Learn Java Programming", "rohizah", new ArrayList<String>(Arrays.asList("daphne", "rebekah")));
-		initializeCourse("TK1964", "Database", "Learn Structured Query Language", "siti", new ArrayList<String>(Arrays.asList("daphne", "rebekah")));
+		initializeCourse("TK2023", "OOSE", "Learn Software Development Life Cycle", "hafiz", new ArrayList<String>(Arrays.asList("rohizah", "faris", "stuart")));
+		initializeCourse("TK1143", "Programming", "Learn Java Programming", "rohizah", new ArrayList<String>(Arrays.asList("siti", "faris", "alven", "stuart")));
+		initializeCourse("TK1964", "Database", "Learn Structured Query Language", "siti", new ArrayList<String>(Arrays.asList("hafiz", "faris")));
 	}
 	
 	private static void initializeStudent(String username, String password, String name,
