fdabe992a3b087ac76ce1b7fb295b737a408a6f2
cutejoy <stuartfuyu0318@gmail.com>
2023-02-09 23:05:52 +0800

    Add initializeQuestion

--- SmartStar/src/Main.java
+++ SmartStar/src/Main.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import controller.Controller;
+import model.Answer;
+import model.Question;
 
 public class Main {
 
@@ -30,6 +32,9 @@ public class Main {
 		initializeCourse("TK2023", "OOSE", "Learn Software Development Life Cycle", "hafiz", new ArrayList<String>(Arrays.asList("rohizah", "faris", "stuart")));
 		initializeCourse("TK1143", "Programming", "Learn Java Programming", "rohizah", new ArrayList<String>(Arrays.asList("siti", "faris", "alven", "stuart")));
 		initializeCourse("TK1964", "Database", "Learn Structured Query Language", "siti", new ArrayList<String>(Arrays.asList("hafiz", "faris")));
+		
+		initializeQuestion("Database Newbie", "What is a database? And what is SQL language used for? Why are some comercial databases(oracle) really expensive and some are free?",
+				"TK1964", "stuart");
 	}
 	
 	private static void initializeStudent(String username, String password, String name,
@@ -51,4 +56,13 @@ public class Main {
 			controller.joinCourse(id);
 		}
 	}
+	
+	private static void initializeQuestion(String title, String content, String courseID, String ownerUsername) {
+		controller.getGlobal().getCourseByCourseID(courseID).addQuestion(new Question(title, content, controller.getGlobal().getCourseByCourseID(courseID), controller.getGlobal().getUserByUsername(ownerUsername)));
+	}
+	
+//	private static void initializeAnswer(String content, String courseID, String questionTitle, String ownerUsername) {
+//		controller.getGlobal().getCourseByCourseID(courseID).getQuestionByTitle(questionTitle).addAnswer(new Answer(content, 
+//				controller.getGlobal().getCourseByCourseID(courseID).getQuestionByTitle(questionTitle), controller.getGlobal().getUserByUsername(ownerUsername)));
+//	}
 }
